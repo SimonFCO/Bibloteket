867a14bc4ee13959a49bbe321087ef7f05eb3bbb
simonFCO <simon.vare6@gmail.com>
2025-10-10 09:38:31 +0000

    I have added the functions Showbooks and borrowBooks so you can now see a list of books and borrow them

--- Bibloteket/Program.cs
+++ Bibloteket/Program.cs
@@ -1,7 +1,14 @@
-﻿using System.Globalization;
+﻿//Simon Brink Sut25
+//
+//Problems
+//Currently i need to remake so that the borrowing system is based on person instead of just one array for all of them
+using System.Globalization;
 
 namespace Bibloteket
 {
+    class books{
+
+    }
     internal class Program
     {
 
@@ -54,7 +61,7 @@ namespace Bibloteket
             {
                 loginAttemps += 1;
 
-                for (int i = 0; i < usernames.length; i++)
+                for (int i = 0; i < usernames.Length; i++)
                 {
                     if(loginUsername == usernames[i] && loginPin == pinCode[i]){
                       loggedIn = true;
@@ -68,19 +75,101 @@ namespace Bibloteket
             }
             
         }
+        
+        //This is the navigationMenu Where after your login you can choose what to do!
+        static void navigationMenu(){
+
+          //1, Visa böcker
+          //2, Låna Bok 
+          //3, Lämna tillbaka Bok
+          //4, Mina lån 
+          //5. Logga ut
+          
+          Console.ReadLine();
+          Console.Clear();
+          Console.WriteLine("1, Visa Böcker");
+          Console.WriteLine("2, Låna bok");
+          Console.WriteLine("3, Lämna tillbaka bok");
+          Console.WriteLine("4, Mina lån");
+          Console.WriteLine("5, Logga ut");
+
+          string answer = Console.ReadLine();
+
+          switch (answer)
+          {
+              case 1:
+              showBooks();
+              break;
+              case 2:
+              borrowBooks();
+              break;
+              case 3:
+              returnBooks();
+              break;
+              case 4:
+              myBorrowedBooks();
+              break;
+              case 5:
+              startMenu();
+              break;
+              default:
+              Console.WriteLine("ogiltigt val");
+              navigationMenu();
+          }
+
+
+        }
+        //This function here will show what books you have, nothing more nothing less
+        static void showBooks(){
+          for (int i = 0; i < bookTitle.Length; i++)
+          {
+              Console.WriteLine($"{i+1} | Titel: {bookTitle[i]} | Exemplar: {aviableBooks[i]}");
+          }
+          Console.WriteLine("Press enter when done");
+          Console.ReadLine( );
+          navigationMenu();
+
+        }
+        //This code is clearly broken please fix one day
+        //This will let the user borrow the books that he chooses
+        static void borrowBooks(){
+          Console.WriteLine("Vilken bok vill du låna? Skriv nummret av boken du vill låna");
+          int answer = int.ConvertToInt32(Console.ReadLine());
+          if(aviableBooks[answer] != 0 &&){
+            aviableBooks[answer] -= 1;
+            isBookBorrowed[answer] == true;
+          }else{
+            Console.WriteLine("You could not borrow that book, there might not be any left");
+          }
+          
+        }
+
+        static void returnBooks(){
+
+        }
+
+        static void myBorrowedBooks(){
+
+        }
+
+
 
         //These are where most of the variables are stored
         static int loginAttemps = 0;
         static string[] usernames = {"Bengt","Stig","Rutger","Sten", "Harald"};
         static int[] pinCode = {123456, 234567, 345678, 987654, 876554 };
         static bool loggedIn;
-        
 
+        //Book variables
+        static string[] bookTitle = {"Harry's potter", "Urtaskig park", "sjärn krig", "stump fiction", "Vargen av väggvägen"};
+        static int[] aviableBooks = {1, 10, 0, 5, 2};
+        static bool[] isBookBorrowed = {false, false, false, false, false};
         //Main Function
         static void Main(string[] args)
         {
 
             tryLogin();
+            navigationMenu();
 
         }
     }
